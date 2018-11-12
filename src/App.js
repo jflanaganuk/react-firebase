ab210d14b1e5c27e881edaf752749b81523a5e45
Joshua Flanagan <joshuaflanagan@joshuas-imac.home>
2018-11-12 19:13:38 +0000

    Adding yarn and package.json files
    Initial work on firebase, uploading files and database paths

--- src/App.js
+++ src/App.js
@@ -1,28 +1,76 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase'
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
+import Storage from './components/Storage'
+
+const config = {
+	apiKey: "AIzaSyD3taQFpVwhb6VOgWTn62gO725-qknCc5Q",
+	authDomain: "uploadr-e33b8.firebaseapp.com",
+	databaseURL: "https://uploadr-e33b8.firebaseio.com",
+	projectId: "uploadr-e33b8",
+	storageBucket: "uploadr-e33b8.appspot.com",
+	messagingSenderId: "322444451251"
+}
+firebase.initializeApp(config)
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+	constructor(props){
+		super(props)
+
+		this.state = {
+			isSignedIn: false
+		}
+
+		this.uiConfig = {
+			signInFlow: 'popup',
+			signInSuccessUrl: '/signedIn',
+			signInOptions: [
+				firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+			],
+			callbacks: {
+				signInSuccessWithAuthResult: () => false
+			}
+		}
+	}
+
+	componentDidMount(){
+		this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
+			(user) => this.setState({isSignedIn: !!user})
+		)
+	}
+
+	componentWillUnmount(){
+		this.unregisterAuthObserver()
+	}
+
+	render() {
+		if (!this.state.isSignedIn) {
+			return(
+				<div className="App">
+					<header className="App-header">
+						<h1>Uploadr</h1>
+						<p>Please sign-in:</p>
+						<StyledFirebaseAuth
+							uiConfig={this.uiConfig}
+							firebaseAuth={firebase.auth()}
+						/>
+					</header>
+				</div>
+			)
+		}
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h1>Uploadr</h1>
+					<p>Welcome {firebase.auth().currentUser.displayName}! You are now signed-in!</p>
+					<Storage/>
+					<button onClick={() => firebase.auth().signOut()}>Sign-out</button>
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/Storage.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react'
+import firebase from 'firebase'
+
+export default class Storage extends Component{
+
+    constructor(props){
+        super(props);
+
+        const database = firebase.firestore();
+        database.settings({
+            timestampsInSnapshots: true
+        });
+
+        const storage = firebase.storage()
+        const storageRef = storage.ref()
+        const imagesRef = storageRef.child(firebase.auth().currentUser.uid)
+
+        this.state = {
+            name: null,
+            file: null,
+            ref: imagesRef,
+            database: database
+        }
+    
+        this.uploadFile = this.uploadFile.bind(this)
+        this.setFile = this.setFile.bind(this)
+        this.setName = this.setName.bind(this)
+        this.addFilePathToDatabase = this.addFilePathToDatabase.bind(this)
+    }
+
+    setName(e){
+        this.setState({name: e.target.value})
+    }
+
+    setFile(e){
+        this.setState({file: e.target.files[0]})
+    }
+
+    uploadFile(){
+        const file = this.state.file
+        console.log(file)
+        this.state.ref.child(this.state.name).put(file).then((snapshot) => {
+            console.log("uploaded file!")
+            this.addFilePathToDatabase();
+        })
+    }
+
+    addFilePathToDatabase(){
+        const formattedDate = new Date();
+
+        this.state.database.collection("files").add({
+            user: firebase.auth().currentUser.uid,
+            date: formattedDate,
+            name: this.state.name
+        })
+        .then((docRef) => {
+            console.log("Document written with ID: ", docRef.id)
+        })
+        .catch((error) => {
+            console.error("Error adding document: ", error)
+        })
+    }
+
+    render(){
+        return (
+            <div>
+                <input type="text" onChange={this.setName}/><br/>
+                <input type="file" onChange={this.setFile}/><br/>
+                <input type="submit" onClick={this.uploadFile}/>
+            </div>
+        )
+    }
+}
