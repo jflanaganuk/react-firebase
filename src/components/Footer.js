9dd9dc0c2ad49387168f600531796193fba4fa91
Joshua Flanagan <joshuaflanagan@joshuas-imac.home>
2018-11-19 18:08:17 +0000

    Added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import styles from './Footer.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCopyright } from '@fortawesome/free-solid-svg-icons'
 
 export default class Footer extends Component{
     render(){
         return(
             <div className={styles.footerContainer}>
-                <div className={styles.footerInfo}></div>
+                <div className={styles.footerInfo}>
+                    <FontAwesomeIcon icon={faCopyright}/> Joshua Flanagan 2018
+                </div>
             </div>
         )
     }
